5d35a73ff78d063f43df7ca42be0582ace63a737
PJ Reiniger <pj.reiniger@gmail.com>
2022-11-11 18:07:16 -0500

    More

--- dependencies/opencv/opencv-java/tests/main.java
+++ dependencies/opencv/opencv-java/tests/main.java
@@ -2,29 +2,18 @@
 
 package bazelrio_test;
 
-import edu.wpi.first.util.CircularBuffer;
-import edu.wpi.first.util.WPIUtilJNI;
-import edu.wpi.first.util.CombinedRuntimeLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class Main {
-    private static final double[] m_values = {
-      751.848, 766.366, 342.657, 234.252, 716.126, 132.344, 445.697, 22.727, 421.125, 799.913
-    };
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(System.getProperty("user.dir"));
-
         
-        CircularBuffer queue = new CircularBuffer(8);
-
-        for (double value : m_values) {
-            queue.addLast(value);
-        }
-
-        System.out.println(queue);
-        System.out.println(CombinedRuntimeLoader.getExtractionDirectory());
-        System.out.println(WPIUtilJNI.now());
-
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
     }
 }
